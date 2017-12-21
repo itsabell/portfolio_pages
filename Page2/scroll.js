46d040bc6ad8050a03b1cd234f33cf1cc9f3910a
itsabell <34717889+itsabell@users.noreply.github.com>
2017-12-21 19:30:47 +0300

    Add files via upload

---
+++ Page2/scroll.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(".menu").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+        document.getElementById('min_men').style.display='none';
+    });
+    $("#fc").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
