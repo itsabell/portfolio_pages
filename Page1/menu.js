1ad4c39eb5dc9df3eeb17aa530343ada0b3a5bb0
itsabell <34717889+itsabell@users.noreply.github.com>
2017-12-21 15:53:02 +0300

    Add files via upload

---
+++ Page1/menu.js
@@ -0,0 +1,8 @@
+function menu() {
+    if(document.getElementById('menu').style.height=='0px'){
+        document.getElementById('menu').style.height='90px';
+    }
+    else {
+        document.getElementById('menu').style.height='0px';
+    }
+}
